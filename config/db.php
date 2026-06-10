5ea49a353ff3c1e91161d0a537f5adef23e73bc2
Zangar <zmbakh@gmail.com>
2026-06-10 10:54:32 +0500

    Adjusted docker compose and db config

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=db;dbname=product_api',
+    'username' => 'yii',
+    'password' => 'secret',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
